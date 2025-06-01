dcfcc0259c463bf40f8bba99d8c934b59333d79c
Aryan Raj <165568975+Aryan1021@users.noreply.github.com>
2025-06-01 19:48:25 +0530

    feat: add User, Post, and Todo models with JSON serialization

---
+++ lib/models/user.dart
@@ -0,0 +1,19 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'user.g.dart';
+
+@JsonSerializable()
+class User {
+  final int id;
+  final String firstName;
+  final String lastName;
+  final String email;
+  final String image;
+
+  User({required this.id, required this.firstName, required this.lastName, required this.email, required this.image});
+
+  String get fullName => '$firstName $lastName';
+
+  factory User.fromJson(Map<String, dynamic> json) => _$UserFromJson(json);
+  Map<String, dynamic> toJson() => _$UserToJson(this);
+}
